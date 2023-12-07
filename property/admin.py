c9880cd779ae15bc77b59b5110f8ca8e43b8dc3c
JacobKleim <yakovkleimenov@yandex.ru>
2023-12-08 01:59:55 +0400

    "Flat" instances in the admin started to show owners

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from property.models import Complaint, Flat, Owner
 
 
+class FlatsInstanceInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ('owner',)
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ["created_at"]
@@ -15,6 +20,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ('liked_by',)
+    inlines = [FlatsInstanceInline]
 
 
 admin.site.register(Flat, FlatAdmin)
